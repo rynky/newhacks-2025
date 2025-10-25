a932c2e3108796f96434073f74300efc840209de
Raiyan <developer.rkbyte@gmail.com>
2025-10-25 17:38:23 -0400

    Added personal trainer prompt

--- superlift/server/common/gemini.js
+++ superlift/server/common/gemini.js
@@ -4,13 +4,13 @@ import { GoogleGenAI } from "@google/genai";
 dotenv.config()
 
 // The client gets the API key from the environment variable GEMINI_API_KEY
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 // Call on Gemini
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Greet the NewHacks audience in one sentence",
+    contents: process.env.GEMINI_PROMPT + "Hi superlift",
   });
   
   console.log(response.text);
