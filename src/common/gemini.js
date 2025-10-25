10380bad9988700a129166b8107889abf80edf53
Raiyan <developer.rkbyte@gmail.com>
2025-10-25 14:19:53 -0400

    Added Gemini API support

---
+++ src/common/gemini.js
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+import { ENVIRONMENT_VARIABLES } from "./constants/envs.js";
+
+// Initialize environment variables
+dotenv.config();
+
+// The client gets the API key from the environment variable GEMINI_API_KEY
+const ai = new GoogleGenAI({apiKey: ENVIRONMENT_VARIABLES.GEMINI_API_KEY});
+
+// Call on Gemini
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Greet the NewHacks audience in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
